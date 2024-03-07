5dcf3d6b8d311aee859f198925f7bd4d5f345c62
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-03-07 01:29:01 +0100

    Create console.py

---
+++ console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+"""
+Module for console
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand console class
+    """
+    prompt = "(hbnb) "
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+      def help_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        print("Quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """
+        EOF (Ctrl+D) signal to exit the program.
+        """
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
